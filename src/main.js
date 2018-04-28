f112df8402d15c3990b6446a56c77229c6652c40
Quan <1020887472@qq.com>
2018-04-28 16:02:04 +0800

    Revert "Develop"

---
+++ src/main.js
@@ -0,0 +1,41 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import Article from './components/Article.vue'
+import ArticleList from './components/Article-List.vue'
+import ArticleContent from './components/Article-content.vue'
+import Worklog from './components/Worklog.vue'
+import WorklogList from './components/Worklog-List.vue'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+/* eslint-disable no-new */
+const routes = [
+  { path: '/', component: ArticleList },
+    { path: '/article', component: Article,
+    children: [
+      { path: '', name: 'article-list', component: ArticleList },
+      { path: ':num', name: 'article-content', component: ArticleContent },
+      { path: 'label/:labelName', name: 'label-article-list', component: ArticleList }
+    ]
+  },
+  { path: '/worklog', component: Worklog,
+    children: [
+      { path: '', name: 'worklog-list', component: WorklogList },
+      { path: ':num', name: 'worklog-content', component: ArticleContent }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes  // === routes: routes
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
